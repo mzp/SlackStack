bd4cf60e6b31ea6898c4dec67bcb5015c9707963
mzp <mzpppp@gmail.com>
2017-04-29 13:34:46 +0900

    :lipstick: fire stroyboard, and use minimum xib

--- Slackdeck/AppDelegate.swift
+++ Slackdeck/AppDelegate.swift
@@ -10,7 +10,7 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    @IBOutlet weak var window : NSWindow!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
