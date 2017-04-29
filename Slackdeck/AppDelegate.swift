1743edc0f687d391d92511826228639a8fb839eb
mzp <mzpppp@gmail.com>
2017-04-29 12:07:27 +0900

    :wrench: create Xcode project

---
+++ Slackdeck/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Slackdeck
+//
+//  Created by mzp on 2017/04/29.
+//  Copyright © 2017 mzp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Slackdeck/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Slackdeck
+//
+//  Created by mzp on 2017/04/29.
+//  Copyright © 2017 mzp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
